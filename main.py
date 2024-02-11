f3648a8bbe4cab1d06b7b3e45c01266b6aac0ccc
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-02-11 21:50:55 +0000

    updates

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+def user_input() -> str:
+    password = input("Please input your password: ")
+    return password
+
+
+def calcuate_strength(password: str) -> int:
+    for digit in password:
+        print(digit)
+
+
+def main():
+    password = user_input()
+    calcuate_strength(password)
 
 
 if __name__ == "__main__":
-    pass
+    main()
